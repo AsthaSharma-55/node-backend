bdcabe34509254b29b5614d26546d960d7a89319
Astha Sharma <aniruddh.a@neosoftmail.com>
2024-02-27 14:22:45 +0530

    updated code

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,5 @@
 const {MongoClient} = require('mongodb')
-const url= 'mongodb://localhost:27017';
+const url= 'mongodb+srv://asthasharmaextern123:fBIcvinzXfMLbxlv@cluster0.aaj3fzz.mongodb.net/';
 const databaseName='e-commerse'
 const client= new MongoClient(url);
 
@@ -11,3 +11,32 @@ async function dbConnect()
   
 }
 module.exports= dbConnect;
+
+
+// const { MongoClient } = require('mongodb');
+
+// const username = 'asthasharmaextern123';
+// const password = '<cX2f9WhA5OpnR9PV>';
+// const clusterName = 'cluster0';
+// const dbName = 'e-commerce';
+
+// const url = `mongodb+srv://${username}:${password}@${clusterName}.aaj3fzz.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+// const client = new MongoClient(url);
+
+// async function dbConnect() {
+//   try {
+//     await client.connect();
+//     console.log('Connected to MongoDB');
+//     const db = client.db();
+//     return db.collection('users');
+//   } catch (error) {
+//     console.error('Error connecting to MongoDB:', error);
+//     throw error; // Rethrow the error for handling in the caller function
+//   }
+// }
+
+// module.exports = dbConnect;
+
+// mongodb+srv://asthasharmaextern123:<fBIcvinzXfMLbxlv>@cluster0.aaj3fzz.mongodb.net/
+// mongodb+srv://asthasharmaextern123:fBIcvinzXfMLbxlv@cluster0.aaj3fzz.mongodb.net/
